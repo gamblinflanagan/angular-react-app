f4fa7cc3e3689d416f6e66e72310d6e495187afb
gamblinflanagan <gamblinflanagan97@gmail.com>
2022-06-29 08:49:59 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import MyReactComponent from './MyReactComponent';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<div [id]="rootId"></div>',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, AfterViewInit, OnDestroy{
   title = 'my-angular-app';
+
+  public rootId = 'rootId'
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.render();
+  }
+
+  ngAfterViewInit() {
+      this.render();
+  }
+
+  ngOnDestroy() {
+  }
+
+  private render() {
+    ReactDOM.render(React.createElement(MyReactComponent), document.getElementById(this.rootId));
+  }
 }
