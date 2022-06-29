19e71aaac9e3fc60ee71d16a9b9dbd43e43ac11e
gamblinflanagan <gamblinflanagan97@gmail.com>
2022-06-29 10:00:43 -0400

    add functionality for multiple components and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,14 @@
-import { AfterViewInit, Component, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
-
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import MyReactComponent from './MyReactComponent';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<div [id]="rootId"></div>',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnChanges, AfterViewInit, OnDestroy{
-  title = 'my-angular-app';
+export class AppComponent {
+  public counter = 21;
 
-  public rootId = 'rootId'
-
-  ngOnChanges(changes: SimpleChanges) {
-    this.render();
-  }
-
-  ngAfterViewInit() {
-      this.render();
-  }
-
-  ngOnDestroy() {
-  }
-
-  private render() {
-    ReactDOM.render(React.createElement(MyReactComponent), document.getElementById(this.rootId));
+  public handleOnClick(stateCounter: number) {
+    this.counter++;
   }
 }
---
+++ src/components/ReactComponents/MyReactComponentWrapper.tsx
@@ -0,0 +1,67 @@
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    EventEmitter,
+    Input,
+    OnChanges,
+    OnDestroy,
+    Output,
+    SimpleChanges,
+    ViewChild,
+    ViewEncapsulation
+  } from '@angular/core';
+  import { MyReactComponent } from './MyReactComponent';
+  
+  import * as React from 'react';
+  import * as ReactDOM from 'react-dom';
+  
+  const containerElementName = 'myReactComponentContainer';
+  
+  @Component({
+    selector: 'app-my-component',
+    template: `<span #${containerElementName}></span>`,
+    styleUrls: ['./MyReactComponent.scss'],
+    encapsulation: ViewEncapsulation.None,
+  })
+  export class MyComponentWrapperComponent implements OnChanges, OnDestroy, AfterViewInit {
+    @ViewChild(containerElementName, {static: true}) containerRef!: ElementRef;
+  
+    @Input() public counter = 10;
+    @Output() public componentClick = new EventEmitter<void>();
+  
+    constructor() {
+      this.handleDivClicked = this.handleDivClicked.bind(this);
+    }
+  
+    public handleDivClicked() {
+      if (this.componentClick) {
+        this.componentClick.emit();
+        this.render();
+      }
+    }
+  
+    ngOnChanges(changes: SimpleChanges): void {
+      this.render();
+    }
+  
+    ngAfterViewInit() {
+      this.render();
+    }
+  
+    ngOnDestroy() {
+      ReactDOM.unmountComponentAtNode(this.containerRef.nativeElement);
+    }
+  
+    private render() {
+      const {counter} = this;
+
+      ReactDOM.render(
+        <React.StrictMode>
+          <div className={'i-am-classy'}>
+              <MyReactComponent counter={counter} onClick={this.handleDivClicked} />
+          </div>
+        </React.StrictMode>
+        , this.containerRef.nativeElement);
+    }
+  }
